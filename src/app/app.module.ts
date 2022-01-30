0b3dcf848e04e9051bb4fb1ccfca96257e3090c3
Jesús Villalobos <jesusvillalobos2807@outlook.es>
2022-01-30 00:46:49 -0700

    user settings form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserSettingsFormsComponent } from './user-settings-forms/user-settings-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSettingsFormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
